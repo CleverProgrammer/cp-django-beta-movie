87acde9e08ab0a33171c8e399fd4745036c1092a
s2krish <krishna.sunuwar@gmail.com>
2018-01-16 15:53:33 +0000

    search fix

--- movies/views.py
+++ movies/views.py
@@ -16,7 +16,7 @@ AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
 # Create your views here.
 def home_page(request):
     user_query = str(request.GET.get('query', ''))
-    search_result = AT.get_all(formula="FIND('" + user_query + "', {Name})")
+    search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({Name}))")
 
     stuff_for_frontend = {"search_result": search_result}
     return render(request, "movies/movies_stuff.html", stuff_for_frontend)
